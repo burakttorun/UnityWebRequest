bb2c3cda3e4701dac4f741c8c00cb0a3d293e807
burakttorun <burakttorun@hotmail.com>
2022-10-17 09:45:15 +0300

    User model added to project

---
+++ UnityWebRequest_AsyncGet/Assets/ThePrototype/User.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User 
+{
+    public int UserId { get; set; }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool Completed { get; set; }
+}
